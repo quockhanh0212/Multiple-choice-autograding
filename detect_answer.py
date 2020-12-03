a7f5c1c6cf0b6f65f031f71080abb608cf3910a6
thinhngo2106 <55101812+thinhngo2106@users.noreply.github.com>
2020-12-03 12:11:12 +0700

    fix detect_answer

--- detect_answer.py
+++ detect_answer.py
@@ -18,31 +18,30 @@ def get_frames(img, centers):
 
     return frames
 
-def convert_to_binary_img(img):
-    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-                                      cv2.THRESH_BINARY, 31, 7)
-    return img
-
 def convert_list_to_array(obj):
     array = np.array(obj, dtype=np.uint8)
     new_image= Image.fromarray(array)
     return np.asarray(new_image)
 
+def convert_to_binary_img(img):
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                                      cv2.THRESH_BINARY, 31, 7)
+    return img
+def pretreatment_template(img):
+    binary = convert_to_binary_img(img)
+    binary = 255 - binary
+    return binary
+
 def template_matching():
     arrayTemplate = []
     templateA = cv2.imread('exam2_A.png', cv2.IMREAD_GRAYSCALE)
-
-    templateA = cv2.adaptiveThreshold(templateA, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-                                      cv2.THRESH_BINARY, 31, 7)
+    templateA = pretreatment_template(templateA)
     templateB = cv2.imread('exam2_B.png', cv2.IMREAD_GRAYSCALE)
-    templateB = cv2.adaptiveThreshold(templateB, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-                                      cv2.THRESH_BINARY, 31, 7)
+    templateB = pretreatment_template(templateB)
     templateC = cv2.imread('exam2_C.png', cv2.IMREAD_GRAYSCALE)
-    templateC = cv2.adaptiveThreshold(templateC, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-                                      cv2.THRESH_BINARY, 31, 7)
+    templateC = pretreatment_template(templateC)
     templateD = cv2.imread('exam2_D.png', cv2.IMREAD_GRAYSCALE)
-    templateD = cv2.adaptiveThreshold(templateD, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-                                      cv2.THRESH_BINARY, 31, 7)
+    templateD = pretreatment_template(templateD)
 
     arrayTemplate.append(templateA)
     arrayTemplate.append(templateB)
@@ -50,19 +49,20 @@ def template_matching():
     arrayTemplate.append(templateD)
     return arrayTemplate
 
+    
 def get_answer(choices):
     arrayTemplate = template_matching()
     answers = []
     dem = 0
     for j in range(len(choices)):
-        min = 0
+        max=0
         k = -1
         for i in range(len(arrayTemplate)):
             choices[j] = convert_to_binary_img(convert_list_to_array(choices[j]))
             result = cv2.matchTemplate(choices[j], arrayTemplate[i], cv2.TM_CCOEFF_NORMED)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-            if min > min_val:
-                min = min_val
+            if max < max_val:
+                max = max_val
                 k = i
         answers.append(k)
         dem += 1
